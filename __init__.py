17877f8bbc0565367cca7828ae9a4050e43327a8
Juan Goyret <j.goyret1475@gmail.com>
2025-07-11 17:23:49 -0300

    adding initial registration nodes

--- __init__.py
+++ __init__.py
@@ -3,10 +3,14 @@
 from .tint_nodes import TintedImageByHexColor
 from .SaveImageWithStructure import SaveImageWithStructure
 from .RecursiveImageLoader import RecursiveImageLoader
+from .SequentialImageLoader import SequentialImageLoader
+from .SequentialSaveImage import SequentialSaveImage
 
 NODE_CLASS_MAPPINGS = {
     "Picta_TintedImageByHexColor": TintedImageByHexColor,
     "Picta_SaveImageWithStructure": SaveImageWithStructure,
     "Picta_RecursiveImageLoader": RecursiveImageLoader,
+    "Picta_SequentialSaveImage": SequentialSaveImage,
+    "Picta_SequentialImageLoader": SequentialImageLoader,
 
 }
